c19531dab044f6abd263ea3a2c1e5fe85067e600
Florian Erler <ethon@ethon.cc>
2017-01-04 12:39:42 +0100

    Added support for workout stats to basic phpbb template

--- logmaker-desktop/src/main/java/cc/ethon/logmaker/gen/model/WorkoutModel.java
+++ logmaker-desktop/src/main/java/cc/ethon/logmaker/gen/model/WorkoutModel.java
@@ -1,5 +1,6 @@
 package cc.ethon.logmaker.gen.model;
 
+import java.text.DecimalFormat;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
@@ -8,6 +9,8 @@ import cc.ethon.logmaker.Workout;
 public class WorkoutModel {
 
 	private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("E, d.L.u", Locale.getDefault());
+	private static final DateTimeFormatter DURATION_FORMATTER = DateTimeFormatter.ofPattern("H'h' m'min'", Locale.getDefault());
+	private static final DecimalFormat WEIGHT_FORMATTER = new DecimalFormat("0.##");
 
 	private final Workout workout;
 
@@ -21,6 +24,18 @@ public class WorkoutModel {
 
 	public String getName() {
 		return workout.getName().isPresent() ? workout.getName().get() : null;
+	}
+
+	public String getDuration() {
+		return workout.getDuration().format(DURATION_FORMATTER);
+	}
+
+	public String getWeightLifted() {
+		return WEIGHT_FORMATTER.format(workout.getWeightLifted());
+	}
+
+	public int getSetCount() {
+		return workout.getSetCount();
 	}
 
 }
